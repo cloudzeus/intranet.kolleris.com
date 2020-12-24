9d13d4fb79f16c58c55f40c38ca87ff6a1369068
mactunechy <mactunechy@gmail.com>
2020-12-24 14:43:27 +0200

    client

---
+++ client/build/precache-manifest.a225a4210e662b678189213d4642da4b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d675d37b523db15695fd34e344dcaf24",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9c4a5241ef1f812cf69c",
+    "url": "/static/css/2.51108769.chunk.css"
+  },
+  {
+    "revision": "61be18e267d64ed24415",
+    "url": "/static/css/main.c761d38c.chunk.css"
+  },
+  {
+    "revision": "9c4a5241ef1f812cf69c",
+    "url": "/static/js/2.7f1fb0ad.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.7f1fb0ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61be18e267d64ed24415",
+    "url": "/static/js/main.42ca5030.chunk.js"
+  },
+  {
+    "revision": "852fd010758ba35a01f8",
+    "url": "/static/js/runtime-main.89091bf7.js"
+  }
+]);
